ebf4893a355a3e6ff02ac284583214779366ad34
amelow <amelielowe96@gmail.com>
2019-01-30 20:04:17 +0100

    HelloWorld

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DA204B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
