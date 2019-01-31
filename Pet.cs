f0c7cc93741b7220538f094355edc9d5f0f56919
amelow <amelielowe96@gmail.com>
2019-01-31 14:14:06 +0100

    created 3 methods

---
+++ Pet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DA204B
+{
+    public class PetApplication {
+         private string name; private int age; private bool isFemale;
+        static void Main(string[] args){
+        
+        Console.WriteLine("works");
+        }
+        private string ReadName(){
+        }
+        private int ReadAge(){
+
+        }
+        private bool CheckIsFemale(){
+            
+        }
+    }
+}
