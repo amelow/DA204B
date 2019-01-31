e060cc19ae0cbf9ab40b683f96799f20e6367497
amelow <amelielowe96@gmail.com>
2019-01-31 15:21:26 +0100

    update

--- Pet.cs
+++ Pet.cs
@@ -4,17 +4,26 @@ namespace DA204B
 {
     public class PetApplication {
          private string name; private int age; private bool isFemale;
+          //ReadName();
         static void Main(string[] args){
-        
-        Console.WriteLine("works");
+    
         }
-        private string ReadName(){
+        public string ReadName(){
+            Console.WriteLine("What is the name of your pet? ");
+            name = Console.ReadLine();
+            return name;
+
         }
-        private int ReadAge(){
+/*        private int ReadAge(){
 
         }
         private bool CheckIsFemale(){
-            
+
         }
+        private static void DisplayInformation(){
+
+        }
+        */
     }
+
 }
