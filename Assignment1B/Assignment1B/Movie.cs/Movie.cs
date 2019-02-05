075fa2229dd14e494d773bb36c81353c7b7f8526
amelow <amelielowe96@gmail.com>
2019-02-05 18:46:49 +0100

    new files for VG tasks

---
+++ Assignment1B/Assignment1B/Movie.cs/Movie.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Movie.cs
+{
+    public class MovieMain
+    {
+        public MovieMain()
+        {
+        }
+    }
+}
---
+++ Assignment1B/Assignment1B/Movie.cs/MovieMain.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Movie.cs
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
