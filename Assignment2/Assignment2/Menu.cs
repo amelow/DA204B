10c98838e9d12aff14fae5a28b246c8f8f4f1f02
amelow <amelielowe96@gmail.com>
2019-02-11 13:31:06 +0100

    Display meny method is done

--- Assignment2/Assignment2/Menu.cs
+++ Assignment2/Assignment2/Menu.cs
@@ -10,6 +10,13 @@ namespace Assignment2
             Console.WriteLine("----------------------------------------");
             Console.WriteLine("\n             MAIN MENU");
             Console.WriteLine("\n ----------------------------------------");
+            Console.WriteLine("Whole numbers with for: 1");
+            Console.WriteLine("Floating point numbers with while: 2");
+            Console.WriteLine("Currency converter with do-while loop: 3");
+            Console.WriteLine("Work schedule: 4");
+            Console.WriteLine("Temperature table: 5");
+            Console.WriteLine("Exit the program: 0");
+            Console.WriteLine("\n ----------------------------------------");
         }
        
     }
