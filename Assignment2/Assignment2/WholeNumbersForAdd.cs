a2cb34bced6a0beb71c52118790c0489d4c588a3
amelow <amelielowe96@gmail.com>
2019-02-11 16:29:33 +0100

    Fixed a small error

--- Assignment2/Assignment2/WholeNumbersForAdd.cs
+++ Assignment2/Assignment2/WholeNumbersForAdd.cs
@@ -4,13 +4,33 @@ namespace Assignment2
     public class WholeNumbersForAdd{
         private int sumOfNumbers;
         private int numOfInput;
+        private int index;
        
          public void Start(){
-            Console.WriteLine("In Class WholeNumbersForAdd");
+            Console.WriteLine("In WholeNumbersForAdd Class");
+            ShowProgramInfo();
             ReadInput();
+            CalculateNumbers();
+            DisplayResult();
+        }
+        private void ShowProgramInfo(){
+            Console.WriteLine("\n\n +++++++++ Summation of whole numbers +++++++++");
+            Console.WriteLine("\n             Using a for-statement \n");
         }
         private void ReadInput(){
+            Console.WriteLine("How many numbers do you want to add?");
+            numOfInput = int.Parse(Console.ReadLine());
+  
+        }
+        private void CalculateNumbers(){
+            for(index = 1; index <= numOfInput; index++){
+                Console.WriteLine("Please give the value number no. " + index + " (whole number) :  ");
+                    sumOfNumbers += int.Parse(Console.ReadLine());
+            }
 
+        }
+        private void DisplayResult(){
+            Console.WriteLine("Sum :"+ sumOfNumbers);
         }
     }
 }
