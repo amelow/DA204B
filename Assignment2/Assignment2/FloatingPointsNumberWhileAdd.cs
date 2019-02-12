49b892103622863e105ec4906298abaaf0fe1ebb
amelow <amelielowe96@gmail.com>
2019-02-12 14:29:33 +0100

    update on the float numbers

--- Assignment2/Assignment2/FloatingPointsNumberWhileAdd.cs
+++ Assignment2/Assignment2/FloatingPointsNumberWhileAdd.cs
@@ -5,7 +5,8 @@ namespace Assignment2
  * Author: Amelie Löwe
  * Date: 2019-02-10. 
  */
-    public class FloatingPointsNumberWhileAdd {
+    public class FloatingPointsNumberWhileAdd
+    {
         /*
          * Repeat to read a number from the user into your program.
          * When the user writes in a zero value, sum up the numbers.
@@ -13,28 +14,40 @@ namespace Assignment2
          * Return to the Main Menu.
          * Mandatory: whileloop & double type for the numbers.        
           */
-        private int sumOfNumbers;
+        private double sumOfNumbers;
+        private bool isDone = false;
+        private double input;
 
-        public void Start() {
+        public void Start()
+        {
             Console.WriteLine("In FloatingPointsNumberWhileAdd Class ");
             ShowProgramInfo();
             ReadInput();
-            CalculateNumbers();
+            //  CalculateNumbers();
             DisplayResult();
         }
-        private void ShowProgramInfo() {
+        private void ShowProgramInfo()
+        {
             Console.WriteLine("\n\n +++++++++ Summation of Float numbers +++++++++");
             Console.WriteLine("\n             Using a while-statement \n");
             Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++++ ");
         }
-        private void ReadInput() {
+        private void ReadInput(){
+            while (!isDone) {
+                Console.Write("Write a number to add to the sum, end with a 0: ");
+                input = double.Parse(Console.ReadLine());
+                sumOfNumbers += input;
 
+                if ((input) == 0.0){
+                    isDone = true;
+                }
+            }
         }
-        private void CalculateNumbers() {
+        private void DisplayResult()
+        {
+            Console.WriteLine("Sum of all numbers:" + sumOfNumbers);
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++++ \n");
 
-        }
-        private void DisplayResult(){
-        
         }
     }
 }
