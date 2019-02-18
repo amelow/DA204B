7ea3486d2ff5c6827c4b59fe0f0521d013775b4b
amelow <amelielowe96@gmail.com>
2019-02-18 13:58:33 +0100

    Comments for the meny class

--- Assignment2/Assignment2/WorkingScheduleMenu.cs
+++ Assignment2/Assignment2/WorkingScheduleMenu.cs
@@ -1,10 +1,16 @@
 ﻿using System;
-namespace Assignment2
-{
-    public class WorkingScheduleMenu
-    {
-        public WorkingScheduleMenu()
-        {
+namespace Assignment2{
+/*
+ * Second assignment for the Introduction to C#(DA204B) course at the University of Malmö.
+ * Author: Amelie Löwe
+ * Date: 2019-02-18. 
+ */
+    public class WorkingScheduleMenu{
+        /*
+         * Following class is complementary to the WorkingSchedule class.
+         * Task description is found in that class.        
+         */        
+        public WorkingScheduleMenu(){
         }
     }
 }
