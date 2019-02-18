38f2fc7e6f0872491aa765c32a7e73c38738ba9b
amelow <amelielowe96@gmail.com>
2019-02-18 13:51:32 +0100

    Comments about the tasks

--- Assignment2/Assignment2/TemperatureTable.cs
+++ Assignment2/Assignment2/TemperatureTable.cs
@@ -1,8 +1,18 @@
 ﻿using System;
-namespace Assignment2
-{
-    public class TemperatureTable
-    {
+namespace Assignment2{
+/*
+ * Second assignment for the Introduction to C#(DA204B) course at the University of Malmö.
+ * Author: Amelie Löwe
+ * Date: 2019-02-18. 
+ */
+    public class TemperatureTable{
+        /*
+         * Write a class that calculates temperatures from 0 to 100 degrees Celsius to corresponding Fahrenheit values.
+         * Show the results as a table rounded to whole numbers,as in the figure shown here.
+         * Formulas used:
+         * toCelsius = 5.0 / 9.0 * (valueToConvert - 32)  
+         * toFahrenheit = 9.0 / 5.0 * valueToConvert + 32.0  
+         */
         public TemperatureTable(){
             Console.WriteLine("In TemperatureTable");
         }
--- Assignment2/Assignment2/WorkingSchedule.cs
+++ Assignment2/Assignment2/WorkingSchedule.cs
@@ -1,8 +1,21 @@
 ﻿using System;
-namespace Assignment2
-{
-    public class WorkingSchedule
-    {
+namespace Assignment2{
+/*
+ * Second assignment for the Introduction to C#(DA204B) course at the University of Malmö.
+ * Author: Amelie Löwe
+ * Date: 2019-02-18. 
+ */
+    public class WorkingSchedule{
+        /*
+         * Following program should display a list of the weeks that the user has to work,the weekends 
+         * and the weeks that she/he should work nights.  
+         * Mandatory for this task:
+         * A new menu
+         * When exiting the menu,the program should return to the Main Menu.
+         * Methods for every task as in the previous sections.
+         * Use the methods of the input class for user input
+         * Write also about motivation for the specific loop type you have used in your code.        
+         */
         public WorkingSchedule(){
             Console.WriteLine("In WorkingSchedule");
         }
