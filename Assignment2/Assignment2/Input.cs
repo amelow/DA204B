533c35f0ce5f94ce16730001d089af6ffa7891a9
amelow <amelielowe96@gmail.com>
2019-02-18 13:35:10 +0100

    Input class, readConsole for int and double done

---
+++ Assignment2/Assignment2/Input.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Assignment2{
+/*
+ * Second assignment for the Introduction to C#(DA204B) course at the University of Malmö.
+ * Author: Amelie Löwe
+ * Date: 2019-02-18. 
+ */
+    public class Input{
+        /*
+        * This class is meant to be a helper class.
+        * Containing shared methods for converting string expression to their corresponding numeric numbers.
+        */
+
+        public static int ReadIntegerConsole() {
+            int number;
+
+            if (int.TryParse(Console.ReadLine(),out number))
+                return number;
+            else
+                Console.WriteLine("Wrong input, please try again!");
+            return ReadIntegerConsole();
+
+        }
+        public static double ReadDoubleConsole(){
+            double number;
+            if (double.TryParse(Console.ReadLine(), out number))
+                return number;
+            else
+                Console.WriteLine("Wrong input, please try again!");
+            return ReadDoubleConsole();
+        }
+    }
+}
